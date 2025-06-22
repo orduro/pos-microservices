8949ced1b63c14466dbf8e86db036046e42e555b
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-06-23 03:16:48 +1000

    docs: implement swagger to health check endpoint and gateway

--- gateway/cmd/api/main.go
+++ gateway/cmd/api/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	_ "github.com/orduro/pos-microservices/gateway/cmd/api/docs"
 	"log"
 )
 
+// @title Gateway API
+// @version 1.0
+// @description This is the gateway that client's will use to access backend resources and services
+
+// @contact.name Austin Sofaer (Backend Engineer)
+// @contact.url www.linkedin.com/in/austinsofaer
+// @contact.email austinsofaer@gmail.com
+
+// @BasePath http://localhost:8080
 func main() {
 	// get config from .env
 	cfg, err := NewConfig()
