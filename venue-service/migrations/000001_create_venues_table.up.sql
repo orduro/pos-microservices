3b230d3b8c19f0ccde249cca4f30b80a5411e545
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-01 16:25:37 +0700

    chore(db): add name address unique constraint

--- venue-service/migrations/000001_create_venues_table.up.sql
+++ venue-service/migrations/000001_create_venues_table.up.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS venues (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    address TEXT NOT NULL,
-    phone TEXT,
-    venue_type TEXT,
-    description TEXT NOT NULL,
-    archived BOOLEAN NOT NULL DEFAULT false,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+   id SERIAL PRIMARY KEY,
+   name TEXT NOT NULL,
+   address TEXT NOT NULL,
+   phone TEXT,
+   venue_type TEXT,
+   description TEXT NOT NULL,
+   archived BOOLEAN NOT NULL DEFAULT false,
+   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   
+   CONSTRAINT unique_venue_name_address UNIQUE (name, address)
 );
-
 CREATE INDEX idx_venues_name ON venues(name);
 CREATE INDEX idx_venues_archived ON venues(archived);
 CREATE INDEX idx_venues_venue_type ON venues(venue_type);
