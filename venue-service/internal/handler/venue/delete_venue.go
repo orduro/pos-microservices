be15920ab4755c482540c5a118bd0d5fc0c8b423
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-05 01:11:38 +0700

    feat: error handling and messages

--- venue-service/internal/handler/venue/delete_venue.go
+++ venue-service/internal/handler/venue/delete_venue.go
@@ -1,31 +1,66 @@
 package venue
 
 import (
-	"fmt"
+	"errors"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/orduro/common/json"
+	"github.com/orduro/pos-microservices/venue/internal/store"
 )
 
 func (h *Handler) ArchiveVenue(w http.ResponseWriter, r *http.Request) {
-	// parse id from URL
 	urlID := chi.URLParam(r, "id")
 	id, err := strconv.ParseInt(urlID, 10, 64)
 	if err != nil {
-		json.WriteError(w, r, http.StatusBadRequest, fmt.Sprintf("cannot parse venue id: %v", urlID))
-		log.Printf("cannot parse venue id %v", urlID)
+		json.WriteError(w, r, http.StatusBadRequest, "invalid venue id")
 		return
 	}
 
-	// update the venue with the corresponding id
-	if err := h.store.Venues.Archive(r.Context(), id); err != nil {
-		json.WriteError(w, r, http.StatusInternalServerError, "unable to archive venue")
-		log.Printf("unable to archive venue: %v", err)
+	err = h.store.Venues.Archive(r.Context(), id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrVenueNotFound):
+			json.WriteError(w, r, http.StatusNotFound, "venue not found")
+			return
+		case errors.Is(err, store.ErrVenueAlreadyArchived):
+			json.WriteError(w, r, http.StatusConflict, "venue already archived")
+			return
+		default:
+			json.WriteError(w, r, http.StatusInternalServerError, "unable to archive venue")
+			log.Printf("unable to archive venue: %v", err)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *Handler) DeleteVenue(w http.ResponseWriter, r *http.Request) {
+	urlID := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(urlID, 10, 64)
+	if err != nil {
+		json.WriteError(w, r, http.StatusBadRequest, "invalid venue id")
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	err = h.store.Venues.Delete(r.Context(), id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrVenueNotFound):
+			json.WriteError(w, r, http.StatusNotFound, "venue not found")
+			return
+		case errors.Is(err, store.ErrVenueNotArchived):
+			json.WriteError(w, r, http.StatusBadRequest, "venue must be archived before deletion")
+			return
+		default:
+			json.WriteError(w, r, http.StatusInternalServerError, "unable to delete venue")
+			log.Printf("unable to delete venue: %v", err)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
