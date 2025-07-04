b023f46a847df55be581357aa02d99ec325dbc0d
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-05 01:09:24 +0700

    feat: add store errors

--- venue-service/internal/store/store_utils.go
+++ venue-service/internal/store/store_utils.go
@@ -1,5 +1,16 @@
 package store
 
-import "github.com/go-playground/validator/v10"
+import (
+	"errors"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var v = validator.New()
+
+var (
+	ErrVenueNotFound        = errors.New("venue not found")
+	ErrVenueAlreadyArchived = errors.New("venue already archived")
+	ErrVenueNotArchived     = errors.New("venue not archived")
+	ErrVenueDuplicate       = errors.New("venue with this name and address already exists")
+)
