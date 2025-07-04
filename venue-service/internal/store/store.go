ae6d847c50d050bb1a32f71934fa36cad3c3fbeb
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-05 01:09:09 +0700

    feat: add delete method to store

--- venue-service/internal/store/store.go
+++ venue-service/internal/store/store.go
@@ -21,4 +21,5 @@ type VenueRepository interface {
 	Create(ctx context.Context, venue *Venue) error
 	GetByID(ctx context.Context, id int64) (*Venue, error)
 	Archive(ctx context.Context, id int64) error
+	Delete(ctx context.Context, id int64) error
 }
