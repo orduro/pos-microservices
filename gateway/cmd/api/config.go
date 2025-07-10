24c195a6764acc8ee364bf7c405ab68bc970fe3b
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-10 21:57:52 +0700

    fix: gateway config not properly pulling env

--- gateway/cmd/api/config.go
+++ gateway/cmd/api/config.go
@@ -9,8 +9,8 @@ import (
 )
 
 type config struct {
-	Addr string `validate:"required"`
-	Env  string `validate:"required,oneof=dev development staging prod production"`
+	Addr string `envconfig:"SERVER_ADDR" validate:"required"`
+	Env  string `envconfig:"ENVIRONMENT" validate:"required,oneof=dev development staging prod production"`
 }
 
 func NewConfig() (*config, error) {
