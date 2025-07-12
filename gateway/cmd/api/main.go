bbb08cc289ead14b2e283fc7920e9abd840e7356
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-12 19:19:27 +0700

    chore: remove swagger comments

--- gateway/cmd/api/main.go
+++ gateway/cmd/api/main.go
@@ -4,15 +4,6 @@ import (
 	"log"
 )
 
-// @title Gateway API
-// @version 1.0
-// @description This is the gateway that client's will use to access backend resources and services
-
-// @contact.name Austin Sofaer (Backend Engineer)
-// @contact.url www.linkedin.com/in/austinsofaer
-// @contact.email austinsofaer@gmail.com
-
-// @BasePath http://localhost:8080
 func main() {
 	// get config from .env
 	cfg, err := NewConfig()
