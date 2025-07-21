5190d4641527653d9963f4178c7c5a040b8067b6
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-22 01:26:17 +0700

    refactor: add jwt configurations to venue service

--- venue-service/cmd/service/config.go
+++ venue-service/cmd/service/config.go
@@ -11,6 +11,7 @@ type config struct {
 	Addr     string        `envconfig:"SERVER_PORT" validate:"required"`
 	Env      string        `envconfig:"ENVIRONMENT" validate:"required,oneof=dev development staging prod production"`
 	Postgres *postgresConf `validate:"required"`
+	JWT      *jwtConf      `validate:"required"`
 }
 
 type postgresConf struct {
@@ -18,6 +19,10 @@ type postgresConf struct {
 	MaxOpenConns int           `envconfig:"POSTGRES_MAX_OPEN_CONNS" validate:"required,min=1"`
 	MaxIdleConns int           `envconfig:"POSTGRES_MAX_IDLE_CONNS" validate:"required,min=0"`
 	MaxIdleTime  time.Duration `envconfig:"POSTGRES_MAX_IDLE_TIME" validate:"required"`
+}
+
+type jwtConf struct {
+	Secret string `envconfig:"JWT_SECRET" validate:"required,min=32"`
 }
 
 func NewConfig() (*config, error) {
