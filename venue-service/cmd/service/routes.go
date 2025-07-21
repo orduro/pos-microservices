f04bef95e73e06d35f839c6a33fbcb4c603b905a
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-22 01:26:43 +0700

    feat: add authentication to venue service

--- venue-service/cmd/service/routes.go
+++ venue-service/cmd/service/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/orduro/common/auth"
 	"github.com/orduro/pos-microservices/venue/internal/handler/health"
 	"github.com/orduro/pos-microservices/venue/internal/handler/venue"
 )
@@ -25,9 +26,12 @@ func (s *server) mount() *chi.Mux {
 	// processing should be stopped.
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	r.Get("/health", healthhandler.Check)
+
 	// prefix /api in front of all routes, all routes go in here
 	r.Route("/api", func(r chi.Router) {
-		r.Get("/health", healthhandler.Check)
+		// make sure secret is correct for /api access
+		r.Use(auth.JWTAuth(s.config.JWT.Secret))
 
 		// routes for /venue
 		r.Route("/venue", func(r chi.Router) {
@@ -42,6 +46,9 @@ func (s *server) mount() *chi.Mux {
 
 			// routes for venue deletes `/venue/delete`
 			r.Route("/delete", func(r chi.Router) {
+				// require admin role
+				r.Use(auth.RequireRole("admin"))
+
 				r.Delete("/{id}", venuehandler.DeleteVenue)
 			})
 
