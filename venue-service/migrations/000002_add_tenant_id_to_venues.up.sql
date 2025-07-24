336c76133493a2bb60e52c84497b39c60941fa48
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-24 19:01:46 +0700

    fix: syntax in db migration

--- venue-service/migrations/000002_add_tenant_id_to_venues.up.sql
+++ venue-service/migrations/000002_add_tenant_id_to_venues.up.sql
@@ -1,4 +1,5 @@
-ALTER TABLE venues ADD COLUMN tenant_id UUID
+ALTER TABLE venues ADD COLUMN tenant_id UUID;
 
 ALTER TABLE venues DROP CONSTRAINT IF EXISTS unique_venue_name_address;
+
 ALTER TABLE venues ADD CONSTRAINT unique_venue_name_address_tenant UNIQUE (name, address, tenant_id);
