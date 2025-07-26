94116bf143b5f4eb9388ba6dd29e25a646e922b0
Austin Sofaer <asof0006@student.monash.edu>
2025-07-26 15:22:50 +0700

    fix: migration down file

--- venue-service/migrations/000002_add_tenant_id_to_venues.down.sql
+++ venue-service/migrations/000002_add_tenant_id_to_venues.down.sql
@@ -1,5 +1,18 @@
+-- remove the tenant-specific unique constraint
 ALTER TABLE venues DROP CONSTRAINT IF EXISTS unique_venue_name_address_tenant;
 
+-- delete duplicates of (name, address) combinations
+-- delete all of them but keep the one with the
+-- smallest id
+DELETE FROM venues 
+WHERE id NOT IN (
+    SELECT MIN(id) 
+    FROM venues 
+    GROUP BY name, address
+);
+
+-- Now we can safely add the old unique constraint
 ALTER TABLE venues ADD CONSTRAINT unique_venue_name_address UNIQUE (name, address);
 
+-- Drop the tenant_id column
 ALTER TABLE venues DROP COLUMN IF EXISTS tenant_id;
