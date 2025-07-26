4275b2e4498cfbcce62ba582c2232c85801f9fd3
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-07-26 22:20:52 +0700

    feat: enable sending password reset email request

--- auth-service/internal/httpclient/send_emails.go
+++ auth-service/internal/httpclient/send_emails.go
@@ -10,7 +10,7 @@ type EmailVerificationRequest struct {
 	VerificationURL string `json:"verification_url"`
 }
 
-func (c *Client) SendCallbackEmail(ctx context.Context, email, callbackURL string) error {
+func (c *Client) SendVerificationEmail(ctx context.Context, email, callbackURL string) error {
 	reqBody := EmailVerificationRequest{
 		Email:           email,
 		VerificationURL: callbackURL,
@@ -19,6 +19,35 @@ func (c *Client) SendCallbackEmail(ctx context.Context, email, callbackURL strin
 	request := ServiceRequest{
 		Method: "POST",
 		Path:   "/api/email/verification",
+		Body:   reqBody,
+	}
+
+	resp, err := c.CallService(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("response from communication service: %+v", resp)
+
+	return nil
+}
+
+type PasswordResetEmailRequest struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	ResetURL string `json:"reset_url"`
+}
+
+func (c *Client) SendPasswordResetEmail(ctx context.Context, email, callbackURL string) error {
+	reqBody := PasswordResetEmailRequest{
+		Email:    email,
+		Username: email,
+		ResetURL: callbackURL,
+	}
+
+	request := ServiceRequest{
+		Method: "POST",
+		Path:   "/api/email/password-reset",
 		Body:   reqBody,
 	}
 
